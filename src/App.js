29fc5aabb28ccfd131653e182cad73dd2a3ed990
Taha Khokharwala <132481345+Taha-khokharwala@users.noreply.github.com>
2024-02-01 17:53:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,3 +20,64 @@ const App = () => {
 
 export default App
 
+// const auth = getAuth(app);
+
+// const App = () => {
+//   const [user, setUser] = useState(null);
+//   const [showSignIn, setShowSignIn] = useState(false);
+
+//   const SignInClick = () => {
+//     setShowSignIn(true);
+//   };
+
+//   const handleLogout = () => {
+//     signOut(auth).then(() => {
+//       alert("LogOut");
+//       setShowSignIn(false);
+//       setUser(null);
+//     });
+//   };
+
+//   useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, (user) => {
+//       if (user) {
+//         setUser(user);
+//       } else {
+//         setUser(null);
+//       }
+//     },[]);
+
+//     return () => unsubscribe();
+//   }, []);
+
+//   if (user === null) {
+//     return (
+//       <div>
+//         {!showSignIn && (
+//           <div>
+//             <SingUp />
+//             <p>
+//               If you have an account, then click on{' '}
+//               <a href='#s' onClick={SignInClick}>
+//                 Sign In
+//               </a>
+//             </p>
+//           </div>
+//         )}
+//         {showSignIn && <SignIn />}
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <div>
+//       <p>
+//         <HomePage /> {user.email}
+//       </p>
+//       <button onClick={handleLogout}>Logout</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+
